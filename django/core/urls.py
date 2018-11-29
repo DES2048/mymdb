19be9241b1689e4f995c90c94caf49afc2d21d5c
Oleg Podzorov <electrochange@mail.ru>
2018-11-29 20:35:20 +0300

    Added movie detail CBV and template. Created base page for site and MovieDetail template extendin it.Main page setting up for CSS Bootstrap

---
+++ django/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "core"
+
+urlpatterns = [
+    path("movies", views.MoviesList.as_view(), name = "MoviesList"),
+    path("movie/<int:pk>", views.MovieDetail.as_view(), name="MovieDetail"),
+  ]
--- django/core/views.py
+++ django/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView # автоклассы жанги для списочного и детального отображения
+
+from core.models import Movie
 
 # Create your views here.
+class MoviesList(ListView):
+  # этот атрибут класса говорит данные какой модкли мы будем использовать для списка
+  model = Movie
+  
+class MovieDetail(DetailView):
+  model = Movie
