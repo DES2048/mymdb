fd579a227d732e147913dd3c129a7c363e0c85e9
oleg <electrochange@mail.ru>
2019-02-06 21:46:04 +0300

    Making relations between Movie and Person - director FK - writers ManyToMany - actors ManyToMany through Role model

--- django/core/models.py
+++ django/core/models.py
@@ -36,6 +36,27 @@ class Movie(models.Model):
     class Meta:
         ordering = ("-year", "title")  # adding base ordering
 
+    director = models.ForeignKey(
+        to="Person",
+        on_delete=models.SET_NULL,
+        related_name="directed",
+        null=True,
+        blank=True
+    )
+
+    writers = models.ManyToManyField(
+        to="Person",
+        related_name="writing_credits",
+        blank=True
+    )
+
+    actors = models.ManyToManyField(
+        to="Person",
+        related_name="acting_credits",
+        through="Role",
+        blank=True
+    )
+
     def __str__(self):
         return "{} ({})".format(self.title, self.year)
 
@@ -47,7 +68,7 @@ class Person(models.Model):
     died = models.DateField(blank=True, null=True)
 
     class Meta:
-        ordering = ("+last_name", "first_name")
+        ordering = ("last_name", "first_name")
 
     def __str__(self):
         if self.died:
@@ -55,3 +76,18 @@ class Person(models.Model):
         else:
             return f"{self.first_name} {self.last_name} ({self.born})"
 
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=140)
+
+    class Meta:
+        unique_together = (
+            "movie",
+            "person",
+            "name",
+        )
+
+    def __str__(self):
+        return f"{self.movie_id} {self.person_id} {self.name}"
